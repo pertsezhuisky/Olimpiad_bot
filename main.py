8b202c75c364d962391525abbb59aa3f80f1fdda
Roman Kizhaev <rkizhaev@yandex.ru>
2023-04-28 18:41:24 +0300

    3

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
+
 from bs4 import BeautifulSoup
 import requests
 
+"""
+НТО, Высшая проба, всесиб, физтех, Олимпиада школьников «Ломоносов», Санкт-Петербургского государственного университета, Всероссийская олимпиада школьников «Высшая проба», 
+Многопредметная олимпиада «Юные таланты», Отраслевая олимпиада школьников «Газпром», Кутафинская олимпиада школьников по праву.
+   
+"""
+lst_olimpiads = [
+                 "https://olimpiada.ru/activity/5369","https://olimpiada.ru/activity/22", "https://olimpiada.ru/activity/125", "https://olimpiada.ru/activity/123",
+                 "https://olimpiada.ru/activity/181", "https://olimpiada.ru/activity/146", "https://olimpiada.ru/activity/233", "https://olimpiada.ru/activity/22",
+                 "https://olimpiada.ru/activity/240", "https://olimpiada.ru/activity/5516", "https://olimpiada.ru/activity/199"
+                 ] 
 
-URL = "https://olimpiada.ru/activity/"
-for event_code in range(1, 10):
-    url = URL + str(event_code)
+
+for url in lst_olimpiads:
     request = requests.get(url)
     bs = BeautifulSoup(request.text, 'html.parser')
     name = bs.find("h1")
     grade = bs.find("span", class_="classes_types_a")
     description = bs.find("div", class_="info block_with_margin_bottom")
     subj = bs.find("div", class_="subject_tags_full")
-    date = bs.find("table", class_="events_for_activity")
+    level_1 = bs.find_all("div", class_="f_blocks")
+    for perch in level_1:
+        if perch.text[3:10] == "Перечне":
+            perch = perch.text.replace("Перечень 2022/23 →", "")
+            level = perch[22:]
+            break
+    print(level)
     try:
-        date_name = date.find("tbody").find_all("div",class_="event_name")
-        date_time = date.find("tbody").find_all("a")
-        specif_label = bs.find('div', id="features").find_all("span")
-        specif = bs.find('div', id="features")
-        dct = {"name":name.text,
-               "date_name":date_name,
-               "date_time":date_time,
-               "grade":grade.text,
-               "description":description.text.replace("\n","").replace("\t","").replace("Еще", "").replace("Свернуть описание", ""),
-               "specifications":specif.text.replace("\n", "").replace("strelka2 Еще факты", "").replace("→", ""),
-               'subjects': subj.text.replace("\n", "").replace("\xa0", " ")
-               }
-        print(dct)
-
+        dct ={
+            "name":name.text,
+            "grade":grade.text,
+            "description":description.text.replace("\n","").replace("\t","").replace("Еще", "").replace("Свернуть описание", "").replace("...", "."),
+            "subjects": subj.text.replace("\n", "").replace("\xa0", " "),
+            "level": level,
+            }
+        print(dct)    
     except:
         pass
