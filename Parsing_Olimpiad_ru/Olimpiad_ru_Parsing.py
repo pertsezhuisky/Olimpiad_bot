bf5df03c156dc625fcb4c1b07485ba07a51cb33c
Roman Kizhaev <rkizhaev@yandex.ru>
2023-05-12 00:03:21 +0300

    Date parsing done. Some URL have bugs

--- Parsing_Olimpiad_ru/Olimpiad_ru_Parsing.py
+++ Parsing_Olimpiad_ru/Olimpiad_ru_Parsing.py
@@ -1,38 +1,52 @@
 import requests
 import re
+import datetime
 
 from bs4 import BeautifulSoup
 from Olimpiad_ALL_LINKS import URL_LIST_ALL_LINKS
+from String_to_Date import str_to_dt_b, str_to_dt_e
 
 
 def get_standart_pages(url):
+    #If mesuaraments are not found on website then default will be used
+    level = 0
+    date_begin = datetime.date(1970,1,1)
+    date_end = datetime.date(1990,1,1)
+
     request = requests.get(url)
     bs = BeautifulSoup(request.text, 'html.parser')
+
     name = bs.find("h1")
+
     dates = bs.find("table", class_="events_for_activity")
-    try:
-        dates_name = dates.find_all("div",class_="event_name")
+    if dates != None:
         dates_numbers = dates.find_all("td", colspan=1)
-        for date in dates_numbers:
-            date_begin = date
-            print(date.text.replace("Прошедшие события", ""), url)
-    except:
-        print("че-то не то", url)
-    subj = bs.find("div", class_="subject_tags_full")
+        dates_labels = dates.find_all("td", colspan=2)
+        date_begin = str_to_dt_b(dates_labels[0].text.replace("Прошедшие события", "")) 
+        date_end = str_to_dt_e(dates_numbers[len(dates_numbers)-1].text.replace("Прошедшие события", "")) 
+        
+        
+    type_olimpiad = "олимпиада"
+    
+   
     level_1 = bs.find_all("div", class_="f_blocks")
     
+    subj = bs.find("div", class_="subject_tags_full")
     grade = bs.find("span", class_="classes_types_a")
   
     for perch in level_1:
         if perch.text[3:10] == "Перечне":
             level = int(perch.text[54:].replace("Перечень 2022/23 →", ""))
-            break
     try:
-        dct ={
+        dct = {
             "name":name.text,
-            "grade":grade.text,
-            "subjects": subj.text.replace("\n", "").replace("\xa0", " "),
+            "begin-date": date_begin,
+            "end-date": date_end,
+            "type": type_olimpiad,
             "level": level,
+            "grade":grade.text,
+            "subjects": subj.text.replace("\n", "").replace("\xa0", " ").replace(" язык", "").split(),
+            
             } 
         return dct
     except:
---
+++ Parsing_Olimpiad_ru/String_to_Date.py
@@ -0,0 +1,35 @@
+import datetime
+
+rus_to_eng_mn = {
+                "янв" :	1,
+                "фев" :	2,
+                "мар" :	3,
+                "апр" :	4,
+                "май" : 5,
+                "июн" : 6,
+                "июл" : 7,
+                "авг" : 8,
+                "сен" : 9,
+                "окт" : 10,
+                "ноя" : 11,
+                "дек" : 12,
+                }
+
+
+
+def str_to_dt_b(dt_b):
+    year = int(str(datetime.datetime.now())[0:4])
+    dt_b = dt_b.replace("..."," ").split()
+    if dt_b[1].isdigit() == True:
+        print(dt_b[2])
+        dt_b_mn = rus_to_eng_mn[dt_b[2]]
+    else:
+        dt_b_mn = rus_to_eng_mn[dt_b[1]]
+    return datetime.date(year, dt_b_mn, int(dt_b[0]))
+    
+def str_to_dt_e(dt_e):
+    year = int(str(datetime.datetime.now())[0:4])
+    dt_e = dt_e.replace("..."," ").split()[0:2]
+    dt_e_mn = rus_to_eng_mn[dt_e[1]]
+    return datetime.date(year, dt_e_mn, int(dt_e[0]))
+    
